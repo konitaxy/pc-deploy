ff85e2e58fc1ade32e74079141985192a8313ba4
BigDuck <haibin.liu@starblaze-tech.com>
2025-08-09 01:53:33 +0800

    backend update

---
+++ front/console/js/pixel-client-legacy.1754675553000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1754675553000 */
+System.register(["../assets/pixel-entry-index-legacy.1754675553000.js"],(function(t,e){"use strict";var n;return{setters:[function(t){n=t.Z}],execute:function(){t("v",(function(t){return n({url:"/client/verifySetting",method:"post",data:t})})),t("g",(function(t){return n({url:"/client/tocp",method:"get",params:t})})),t("c",(function(t){return n({url:"/client/tocp",method:"post",data:t})}))}}}));
---
+++ front/console/js/pixel-reload.1754675553000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1754675553000 */
+import{z as s,d as e,j as a}from"../assets/pixel-entry-index.1754675553000.js";const t={name:"Reload"},o=Object.assign(t,{setup:t=>(s().go(-1),(s,t)=>(a(),e("div")))});export{o as default};
