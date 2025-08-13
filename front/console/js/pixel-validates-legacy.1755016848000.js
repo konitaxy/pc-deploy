74f50172ff8f0413428de154da2071dd496985f4
BigDuck <haibin.liu@starblaze-tech.com>
2025-08-13 19:29:46 +0800

    backend update

--- front/console/js/pixel-validates-legacy.1755016848000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on melong-console 
- Time : 1755016848000 */
-System.register(["../assets/pixel-entry-index-legacy.1755016848000.js"],(function(r,a){"use strict";var e;return{setters:[function(r){e=r.u}],execute:function(){r("v",(function(r,a,t){var n=e();a?/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(a)?t():t(new Error(n.t("lang.validation.email_invalid"))):t(new Error(n.t("lang.validation.email_required")))})),r("a",(function(r,a,t){var n=e();a?a.length<8?t(new Error(n.t("lang.validation.password_length"))):/(?=.*[a-z])/.test(a)?/(?=.*[A-Z])/.test(a)?t():t(new Error(n.t("lang.validation.password_uppercase"))):t(new Error(n.t("lang.validation.password_lowercase"))):t(new Error(n.t("lang.validation.password_required")))}))}}}));
