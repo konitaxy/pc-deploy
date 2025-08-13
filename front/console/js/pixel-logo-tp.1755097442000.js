2418ee2d80d06a3fccacf8f3eb5f486b1c1fa461
BigDuck <haibin.liu@starblaze-tech.com>
2025-08-13 23:04:52 +0800

    backend update

---
+++ front/console/js/pixel-logo-tp.1755097442000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1755097442000 */
+const e=""+new URL("../assets/pixel-logo-tp.1755097442000.png",import.meta.url).href;export{e as _};
---
+++ front/console/js/pixel-logo.1755097442000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1755097442000 */
+const e=""+new URL("../assets/pixel-logo.1755097442000.png",import.meta.url).href;export{e as l};
