7a16ade35cf4aa3510c59b657f8f083c0a924365
BigDuck <haibin.liu@starblaze-tech.com>
2025-08-15 00:42:49 +0800

    backend update

---
+++ front/console/js/pixel-logo-legacy.1755189182000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1755189182000 */
+System.register([],function(e,t){"use strict";return{execute:function(){e("l",""+new URL("../assets/pixel-logo.1755189182000.png",t.meta.url).href)}}});
