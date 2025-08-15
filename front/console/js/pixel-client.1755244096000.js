f35e71fde3b07704e9656cb45754cbfe2a709502
BigDuck <haibin.liu@starblaze-tech.com>
2025-08-15 16:20:37 +0800

    backend update

--- front/console/js/pixel-client.1755244096000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on melong-console 
- Time : 1755244096000 */
-import{$ as t}from"../assets/pixel-entry-index.1755244096000.js";const e=e=>t({url:"/client/verifySetting",method:"post",data:e}),a=e=>t({url:"/client/tocp",method:"get",params:e}),o=e=>t({url:"/client/tocp",method:"post",data:e}),s=e=>t({url:"/client/tocp",method:"delete",data:e});export{o as c,s as d,a as g,e as v};
---
+++ front/console/js/pixel-transactionDetails.vue_vue_type_style_index_0_scoped_bf407b4e_lang-legacy.1755245963000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1755245963000 */
+System.register([],function(e,t){"use strict";return{execute:function(){var e=document.createElement("style");e.textContent=".container[data-v-bf407b4e]{padding:20px}\n/*$vite$:1*/",document.head.appendChild(e)}}});
