aa586e20fd2a515a48dcef6c4381083d53e1fd0a
BigDuck <haibin.liu@starblaze-tech.com>
2025-08-16 16:02:54 +0800

    backend update

---
+++ front/console/js/pixel-logo-tp.1755331324000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1755331324000 */
+const e=""+new URL("../assets/pixel-logo-tp.1755331324000.png",import.meta.url).href;export{e as _};
---
+++ front/console/js/pixel-transactionDetails.vue_vue_type_style_index_0_scoped_df552005_lang-legacy.1755331324000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1755331324000 */
+System.register([],function(e,t){"use strict";return{execute:function(){var e=document.createElement("style");e.textContent=".container[data-v-df552005]{padding:20px}\n/*$vite$:1*/",document.head.appendChild(e)}}});
