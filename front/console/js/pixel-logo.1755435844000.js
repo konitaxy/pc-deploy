4bc8d2aa6551b72b09c9a335a4635cb2f4c770ba
BigDuck <haibin.liu@starblaze-tech.com>
2025-08-17 22:27:14 +0800

    backend update

--- front/console/js/pixel-logo.1755435844000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on melong-console 
- Time : 1755435844000 */
-const e=""+new URL("../assets/pixel-logo.1755435844000.png",import.meta.url).href;export{e as l};
---
+++ front/console/js/pixel-rechargeAndWithdraw.1755440749000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1755440749000 */
+import e from"./pixel-index.175544074900019.js";import l from"./pixel-index.175544074900017.js";import{g as a,d as t,j as r,f as i,w as p}from"../assets/pixel-entry-index.1755440749000.js";import"./pixel-withdraw_form.1755440749000.js";import"./pixel-finance.1755440749000.js";import"./pixel-clipboard-polyfill.es6.1755440749000.js";import"./pixel-format.1755440749000.js";import"./pixel-date.1755440749000.js";import"./pixel-recharge_form.1755440749000.js";const s={__name:"rechargeAndWithdraw",setup:s=>(s,o)=>{const m=a("el-tab-pane"),n=a("el-tabs");return r(),t("div",null,[i(n,null,{default:p(()=>[i(m,{label:s.$t("lang.wallet_recharge")},{default:p(()=>[i(l)]),_:1},8,["label"]),i(m,{label:s.$t("lang.wallet_withdrawal")},{default:p(()=>[i(e)]),_:1},8,["label"])]),_:1})])}};export{s as default};
