01aff8638c721d847219299032d4d1908983cab6
BigDuck <haibin.liu@starblaze-tech.com>
2025-08-18 15:22:52 +0800

    backend update

--- front/console/js/pixel-transactionDetails.vue_vue_type_style_index_0_scoped_0a4bbea5_lang-legacy.1755492845000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on melong-console 
- Time : 1755492845000 */
-System.register([],function(e,t){"use strict";return{execute:function(){var e=document.createElement("style");e.textContent=".container[data-v-0a4bbea5]{padding:20px}\n/*$vite$:1*/",document.head.appendChild(e)}}});
---
+++ front/console/js/pixel-transactionDetails.vue_vue_type_style_index_0_scoped_0a4bbea5_lang-legacy.1755501702000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1755501702000 */
+System.register([],function(e,t){"use strict";return{execute:function(){var e=document.createElement("style");e.textContent=".container[data-v-0a4bbea5]{padding:20px}\n/*$vite$:1*/",document.head.appendChild(e)}}});
