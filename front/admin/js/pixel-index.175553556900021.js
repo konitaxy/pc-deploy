81b423c0071692c8d3d6a0359705734ac0a7441b
BigDuck <haibin.liu@starblaze-tech.com>
2025-08-19 13:07:43 +0800

    backend update

---
+++ front/admin/js/pixel-index.175553556900021.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1755535569000 */
+import{_ as r,o as a,c as e}from"../gva/pixel-index.1755535569000.js";const o={class:"container"};var n=r({},[["render",function(r,n){return a(),e("div",o," hello world ")}]]);export{n as default};
---
+++ front/console/js/pixel-logo.1755580010000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1755580010000 */
+const e=""+new URL("../assets/pixel-logo.1755580010000.png",import.meta.url).href;export{e as l};
