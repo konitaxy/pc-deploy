3e28169f32795980cd7215e8a7276d4d0d54e923
BigDuck <haibin.liu@starblaze-tech.com>
2025-08-20 22:53:25 +0800

    backend update

--- front/console/js/pixel-transactionDetails.vue_vue_type_style_index_0_scoped_a537a150_lang-legacy.1755588136000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on melong-console 
- Time : 1755588136000 */
-System.register([],function(e,t){"use strict";return{execute:function(){var e=document.createElement("style");e.textContent=".container[data-v-a537a150]{padding:20px}\n/*$vite$:1*/",document.head.appendChild(e)}}});
