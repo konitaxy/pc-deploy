ff8937858f114b5b862380da0e74213025ded19e
BigDuck <haibin.liu@starblaze-tech.com>
2025-08-21 17:09:46 +0800

    backend update

---
+++ front/console/js/pixel-index-legacy.175576733600012.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1755767336000 */
+System.register(["../assets/pixel-entry-index-legacy.1755767336000.js"],function(e,n){"use strict";var t,r,u,i,l,a,o,c,s,f,d;return{setters:[function(e){t=e.K,r=e.r,u=e.c,i=e.o,l=e.e,a=e.w,o=e.T,c=e.l,s=e.N,f=e.g,d=e.O}],execute:function(){var n={name:"Profile"};e("default",Object.assign(n,{setup:function(e){var n=t();return function(e,t){var v=r("router-view");return i(),u("div",null,[l(v,null,{default:a(function(e){var t=e.Component;return[l(o,{mode:"out-in",name:"el-fade-in-linear"},{default:a(function(){return[(i(),c(s,{include:f(n).keepAliveRouters},[(i(),c(d(t)))],1032,["include"]))]}),_:2},1024)]}),_:1})])}}}))}}});
--- front/console/js/pixel-index.175576706600012.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on melong-console 
- Time : 1755767066000 */
-import{K as e,r as s,c as a,o as n,e as t,w as l,T as o,l as r,N as i,g as u,O as d}from"../assets/pixel-entry-index.1755767066000.js";const c={name:"Profile"},f=Object.assign(c,{setup(c){const f=e();return(e,c)=>{const m=s("router-view");return n(),a("div",null,[t(m,null,{default:l(({Component:e})=>[t(o,{mode:"out-in",name:"el-fade-in-linear"},{default:l(()=>[(n(),r(i,{include:u(f).keepAliveRouters},[(n(),r(d(e)))],1032,["include"]))]),_:2},1024)]),_:1})])}}});export{f as default};
