d5a92574737ff827c502ce21ade61c207cb9ae0d
BigDuck <haibin.liu@starblaze-tech.com>
2025-08-23 17:43:51 +0800

    backend update

--- front/admin/js/pixel-reload.1755621258000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on gin-vue-admin 
- Time : 1755621258000 */
-import{u as a,o as s,c as e}from"../gva/pixel-index.1755621258000.js";const o=Object.assign({name:"Reload"},{setup:o=>(a().go(-1),(a,o)=>(s(),e("div")))});export{o as default};
