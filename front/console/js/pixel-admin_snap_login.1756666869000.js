c3eedd428bc689dde17009497fc4b2b00eae5f74
BigDuck <haibin.liu@starblaze-tech.com>
2025-09-01 03:07:12 +0800

    backend update

--- front/console/js/pixel-admin_snap_login.1756666869000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on melong-console 
- Time : 1756666869000 */
-import{k as e,j as a,q as o,z as t,y as n,E as s,c as i,e as l,b as r,w as d,r as p,o as c}from"../assets/pixel-entry-index.1756666869000.js";import m from"./pixel-index.17566668690004.js";import"./pixel-logo-tp.1756666869000.js";import"./pixel-screenfull.1756666869000.js";const u={__name:"admin_snap_login",setup(u){const w=e(),g=o();return a(()=>{g.params.code?t({code:g.params.code}).then(e=>{if(0==e.code){if(window.localStorage.getItem("token")===e.data)return void window.location.replace("/");window.localStorage.setItem("token",e.data),w.setToken(e.data),n.push({name:"Login",replace:!0}),window.location.reload()}}):s.error("Invalid to Login")}),(e,a)=>{const o=p("el-header");return c(),i("div",null,[l(o,null,{default:d(()=>[l(m)]),_:1}),a[0]||(a[0]=r("h1",null,"wait for logining",-1))])}}};export{u as default};
--- front/console/js/pixel-index.17566668690006.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on melong-console 
- Time : 1756666869000 */
-import{s as e}from"./pixel-screenfull.1756666869000.js";import{_ as s,j as a,Q as l,h as n,c as t,o as c}from"../assets/pixel-entry-index.1756666869000.js";const i={key:0,class:"gvaIcon gvaIcon-fullscreen-expand"},o={key:1,class:"gvaIcon gvaIcon-fullscreen-shrink"},r={name:"Screenfull"},d=s(Object.assign(r,{props:{width:{type:Number,default:22},height:{type:Number,default:22},fill:{type:String,default:"#48576a"}},setup(s){a(()=>{e.isEnabled&&e.on("change",u)}),l(()=>{e.off("change")});const r=()=>{e.isEnabled&&e.toggle()},d=n(!0),u=()=>{d.value=!e.isFullscreen};return(e,s)=>(c(),t("div",{onClick:r},[d.value?(c(),t("div",i)):(c(),t("div",o))]))}}),[["__scopeId","data-v-cdd0729b"]]);export{d as default};
