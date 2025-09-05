ad9befdfb3df1e49a2f75ebf72ca74f33cb6c2db
BigDuck <haibin.liu@starblaze-tech.com>
2025-09-06 00:39:43 +0800

    backend update

--- front/admin/js/pixel-card.1756130956000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on gin-vue-admin 
- Time : 1756130956000 */
-import{s as a}from"../gva/pixel-index.1756130956000.js";const d=d=>a({url:"/admin/card/cardBin/add",method:"post",data:d}),t=d=>a({url:"/admin/card/cardBin/list",method:"post",data:d}),s=d=>a({url:"/admin/card/list",method:"post",data:d}),r=d=>a({url:"/admin/card/transaction/list",method:"post",data:d}),o=d=>a({url:"/admin/card/cancel",method:"post",data:d}),i=d=>a({url:"admin/card/sync",method:"post",data:d});export{d as a,s as b,o as c,r as d,t as l,i as s};
