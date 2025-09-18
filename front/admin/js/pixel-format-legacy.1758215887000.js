3ddf067809deb1d9f7c13306bf150bb5b550ed8c
BigDuck <haibin.liu@starblaze-tech.com>
2025-09-19 01:19:12 +0800

    backend update

---
+++ front/admin/js/pixel-format-legacy.1758215887000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1758215887000 */
+System.register(["./pixel-dictionary-legacy.1758215887000.js"],(function(n){"use strict";var t;return{setters:[function(n){t=n.f}],execute:function(){n("b",(function(n){return null!==n?n?"是":"否":""})),n("a",(function(n){if(null!==n&&""!==n&&void 0!==n){var e=new Date(n);return t(e,"yyyy-MM-dd hh:mm:ss")}return""})),n("f",(function(n){if(null!==n&&""!==n&&void 0!==n){var e=new Date(n);return t(e,"yyyy-MM-dd")}return""}))}}}));
