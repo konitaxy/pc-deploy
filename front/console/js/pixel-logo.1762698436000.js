fc2b3a04faa2d1e627d0623ba74da0f59c09c36b
BigDuck <bigduck@gmail.com>
2025-12-07 21:30:14 +0800

    backend update

--- front/console/js/pixel-logo.1762698436000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on melong-console 
- Time : 1762698436000 */
-const e=""+new URL("../assets/pixel-logo.1762698436000.png",import.meta.url).href;export{e as l};
---
+++ front/console/js/pixel-logo.1765114141000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1765114141000 */
+const e=""+new URL("../assets/pixel-logo.1765114141000.png",import.meta.url).href;export{e as l};
