bf08d89fac22237bf760b7c2657c06b8c52525a8
BigDuck <bigduck@gmail.com>
2025-12-17 18:24:38 +0800

    backend update

--- front/admin/js/pixel-format-legacy.1763473713000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on gin-vue-admin 
- Time : 1763473713000 */
-System.register(["./pixel-dictionary-legacy.1763473713000.js"],(function(n){"use strict";var t;return{setters:[function(n){t=n.f}],execute:function(){n("b",(function(n){return null!==n?n?"是":"否":""})),n("a",(function(n){if(null!==n&&""!==n&&void 0!==n){var e=new Date(n);return t(e,"yyyy-MM-dd hh:mm:ss")}return""})),n("f",(function(n){if(null!==n&&""!==n&&void 0!==n){var e=new Date(n);return t(e,"yyyy-MM-dd")}return""}))}}}));
--- front/admin/js/pixel-reload.1763473713000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on gin-vue-admin 
- Time : 1763473713000 */
-import{u as a,o as s,c as e}from"../gva/pixel-index.1763473713000.js";const o=Object.assign({name:"Reload"},{setup:o=>(a().go(-1),(a,o)=>(s(),e("div")))});export{o as default};
