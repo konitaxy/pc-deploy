86c787cda6e3497c6a8dc490a8cb3b063809e312
BigDuck <bigduck@gmail.com>
2026-02-27 18:48:05 +0800

    backend update

---
+++ front/console/js/pixel-logo-legacy.1772189175000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1772189175000 */
+System.register([],function(e,t){"use strict";return{execute:function(){e("l",""+new URL("../assets/pixel-logo.1772189175000.png",t.meta.url).href)}}});
--- front/console/js/pixel-logo.1772175347000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on melong-console 
- Time : 1772175347000 */
-const e=""+new URL("../assets/pixel-logo.1772175347000.png",import.meta.url).href;export{e as l};
