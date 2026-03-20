057f9985ad0354ae59f939e6ec965f870f6a92f9
BigDuck <bigduck@gmail.com>
2026-03-20 18:45:26 +0800

    backend update

---
+++ front/admin/js/pixel-index-legacy.177400340600021.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1774003406000 */
+System.register(["../gva/pixel-index-legacy.1774003406000.js"],(function(e){"use strict";var t,n,r;return{setters:[function(e){t=e._,n=e.o,r=e.c}],execute:function(){var c={class:"container"};e("default",t({},[["render",function(e,t){return n(),r("div",c," hello world ")}]]))}}}));
--- front/admin/js/pixel-reload.1770808323000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on gin-vue-admin 
- Time : 1770808323000 */
-import{u as a,o as s,c as e}from"../gva/pixel-index.1770808323000.js";const o=Object.assign({name:"Reload"},{setup:o=>(a().go(-1),(a,o)=>(s(),e("div")))});export{o as default};
