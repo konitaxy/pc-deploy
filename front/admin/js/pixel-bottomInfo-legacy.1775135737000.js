9b8048e072e115fc00ff0204b9f16653af0886c8
BigDuck <bigduck@gmail.com>
2026-04-02 22:19:05 +0800

    backend update

---
+++ front/admin/js/pixel-bottomInfo-legacy.1775135737000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1775135737000 */
+System.register(["./pixel-bottomInfo.vue_vue_type_style_index_0_lang-legacy.1775135737000.js","../gva/pixel-index-legacy.1775135737000.js"],(function(e){"use strict";var n,t,o,u;return{setters:[function(){},function(e){n=e._,t=e.o,o=e.c,u=e.d}],execute:function(){var i={class:"bottom-info"};e("default",n({name:"BottomInfo"},[["render",function(e,n,l,r,s,a){return t(),o("div",i,n[0]||(n[0]=[u("div",null,[u("span",null,"Powered by"),u("span",null," 路漫漫 ")],-1)]))}]]))}}}));
---
+++ front/admin/js/pixel-index.177513573700021.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1775135737000 */
+import{_ as r,o as a,c as e}from"../gva/pixel-index.1775135737000.js";const o={class:"container"};var n=r({},[["render",function(r,n){return a(),e("div",o," hello world ")}]]);export{n as default};
