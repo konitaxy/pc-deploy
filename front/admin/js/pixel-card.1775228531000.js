77cc162f345bac57ea5b4f9a7e634755910a3125
BigDuck <bigduck@gmail.com>
2026-04-03 23:09:51 +0800

    backend update

---
+++ front/admin/js/pixel-card.1775228531000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1775228531000 */
+import{s as a}from"../gva/pixel-index.1775228531000.js";const d=d=>a({url:"/admin/card/cardBin/add",method:"post",data:d}),t=d=>a({url:"/admin/card/cardBin/list",method:"post",data:d}),s=d=>a({url:"/admin/card/list",method:"post",data:d}),r=d=>a({url:"/admin/card/transaction/list",method:"post",data:d}),o=d=>a({url:"/admin/card/cancel",method:"post",data:d}),i=d=>a({url:"admin/card/sync",method:"post",data:d}),m=d=>a({url:"/admin/card/frozen",method:"post",data:d});export{d as a,s as b,o as c,r as d,m as f,t as l,i as s};
--- front/admin/js/pixel-reload.1775135737000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on gin-vue-admin 
- Time : 1775135737000 */
-import{u as a,o as s,c as e}from"../gva/pixel-index.1775135737000.js";const o=Object.assign({name:"Reload"},{setup:o=>(a().go(-1),(a,o)=>(s(),e("div")))});export{o as default};
