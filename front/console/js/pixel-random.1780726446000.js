7dc83ff4eae92ec694feee545d9e491d3d76d6a7
BigDuck <bigduck@gmail.com>
2026-06-06 14:15:33 +0800

    backend update

---
+++ front/console/js/pixel-random.1780726446000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1780726446000 */
+const t=()=>{const t=(new Date).getFullYear(),a=t-55,o=t-20,r=Math.floor(Math.random()*(o-a+1))+a,n=String(Math.floor(12*Math.random())+1).padStart(2,"0"),c=String(Math.floor(28*Math.random())+1).padStart(2,"0");return"".concat(r,"-").concat(n,"-").concat(c)};export{t as r};
--- front/console/js/pixel-rechargeAndWithdraw.1780726145000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on melong-console 
- Time : 1780726145000 */
-import e from"./pixel-index.178072614500021.js";import l from"./pixel-index.178072614500019.js";import{r as a,c as t,o as r,d as i,w as p}from"../assets/pixel-entry-index.1780726145000.js";import"./pixel-withdraw_form.1780726145000.js";import"./pixel-finance.1780726145000.js";import"./pixel-clipboard-polyfill.es6.1780726145000.js";import"./pixel-format.1780726145000.js";import"./pixel-date.1780726145000.js";import"./pixel-excel.1780726145000.js";import"./pixel-recharge_form.1780726145000.js";const s={__name:"rechargeAndWithdraw",setup:s=>(s,o)=>{const m=a("el-tab-pane"),n=a("el-tabs");return r(),t("div",null,[i(n,null,{default:p(()=>[i(m,{label:s.$t("lang.wallet_recharge")},{default:p(()=>[i(l)]),_:1},8,["label"]),i(m,{label:s.$t("lang.wallet_withdrawal")},{default:p(()=>[i(e)]),_:1},8,["label"])]),_:1})])}};export{s as default};
